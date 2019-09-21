326a85112c06ab50efdaa7a0918695727ccdc3e2
jasminexz <491548320@qq.com>
2019-09-22 01:38:42 +0800

    全量提交

---
+++ learing-java/src/main/java/com/jasmine/java/LearingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingJavaApplication.class, args);
+	}
+
+}
---
+++ learing-springboot/src/main/java/com/jasmine/learingsb/LearingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.learingsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingSpringbootApplication.class, args);
+	}
+
+}
