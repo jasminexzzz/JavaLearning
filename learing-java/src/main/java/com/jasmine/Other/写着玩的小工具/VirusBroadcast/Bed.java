d5718617b74f94fbd2814ee0c0e0b3a745904dd6
jasminexz <491548320@qq.com>
2020-02-05 00:45:41 +0800

    TEST

---
+++ learing-java/src/main/java/com/jasmine/Other/写着玩的小工具/VirusBroadcast/Bed.java
@@ -0,0 +1,22 @@
+package com.jasmine.Other.写着玩的小工具.VirusBroadcast;
+
+/**
+ * @ClassName: Bed
+ * @Description: TODO
+ * @author: Bruce Young
+ * @date: 2020年02月02日 21:00
+ */
+public class Bed extends Point{
+    public Bed(int x, int y) {
+        super(x, y);
+    }
+    private boolean isEmpty=true;
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
+    }
+}
---
+++ learing-java/src/main/java/com/jasmine/Other/写着玩的小工具/VirusBroadcast/City.java
@@ -0,0 +1,33 @@
+package com.jasmine.Other.写着玩的小工具.VirusBroadcast;
+
+/**
+ * @ClassName: City
+ * @Description: TODO
+ * @author: Bruce Young
+ * @date: 2020年02月02日 17:48
+ */
+public class City {
+    private int centerX;
+    private int centerY;
+
+    public City(int centerX, int centerY) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+}
