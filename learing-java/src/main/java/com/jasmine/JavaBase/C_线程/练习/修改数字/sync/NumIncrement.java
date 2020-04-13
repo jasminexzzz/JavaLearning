079f2e0d50ef53e2656fa1f74713c55408e81b0d
wangyf <491548320@qq.com>
2020-04-13 20:36:49 +0800

    提交

--- learing-java/src/main/java/com/jasmine/JavaBase/C_线程/练习/修改数字/sync/NumIncrement.java
+++ learing-java/src/main/java/com/jasmine/JavaBase/C_线程/练习/修改数字/sync/NumIncrement.java
@@ -1,34 +1,79 @@
 package com.jasmine.JavaBase.C_线程.练习.修改数字.sync;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 多线程把数字加到100
  */
 @SuppressWarnings("all")
 public class NumIncrement implements Runnable {
-    private int num;
+    private int num = 0;
+    private CountDownLatch latch;
+    private List<Integer> list;
 
-    private synchronized void sync(){
-        System.out.println(Thread.currentThread().getName() + ":" + ++num);
+    public NumIncrement (CountDownLatch latch,List<Integer> list) {
+        this.latch = latch;
+        this.list = list;
+    }
+
+    private synchronized void sync() {
+        num++;
     }
 
     private void notSync(){
-        System.out.println("run --> "+Thread.currentThread().getName() + ":" + ++num);
+        list.add(1);
     }
 
     @Override
     public void run() {
-        for (int i = 1 ; i <= 50 ; i++ ) {
-            sync();
+        for (int i = 0 ; i < 50 ; i++ ) {
+            notSync();
         }
+        latch.countDown();
+
     }
 
+    public List<Integer> getList() {
+        return list;
+    }
 
     public static void main(String[] args) {
-        NumIncrement st = new NumIncrement();     // ①
-        // 通过new Thread(target , name)方法创建新线程
-        new Thread(st , "新线程1").start();
-        new Thread(st , "新线程2").start();
+        final CountDownLatch latch = new CountDownLatch(40);
+        NumIncrement st = new NumIncrement(latch,new ArrayList<>());
+
+        for(int i = 0 ; i < 40 ; i++) {
+            Thread t = new Thread(st,"T" + i);
+            t.start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(st.getList().size());
+
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(5,5,100, TimeUnit.MILLISECONDS,new ArrayBlockingQueue<>(5));
+//        executor.execute(st);
+//        executor.execute(st);
+//        executor.execute(st);
+//        executor.execute(st);
+//        executor.execute(st);
+//
+//        executor.shutdown();
+//        while (true) {
+//            if (executor.isTerminated()) {
+//                System.out.println(st.getNum());
+//                break;
+//            }
+//        }
     }
 }
 
