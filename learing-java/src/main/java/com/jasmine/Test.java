3ea0729789c558f26cf8361a58abf7b065a58843
wangyf <491548320@qq.com>
2020-04-24 20:46:04 +0800

    提交

---
+++ learing-java/src/main/java/com/jasmine/Test.java
@@ -0,0 +1,10 @@
+package com.jasmine;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        double val = 11.5;
+        System.out.println(Math.round(11.5));
+    }
+}
--- learing-java/src/main/java/com/jasmine/设计模式/单例模式_Singleton/懒汉式/Singleton.java
+++ learing-java/src/main/java/com/jasmine/设计模式/单例模式_Singleton/懒汉式/Singleton.java
@@ -1,7 +1,5 @@
 package com.jasmine.设计模式.单例模式_Singleton.懒汉式;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * 懒汉式单例
@@ -64,8 +62,9 @@ public class Singleton {
     public static Singleton getInstance线程安全调优双重检查() {
         if(single == null){// 1
             synchronized (Singleton.class){
-                if(single == null)
+                if(single == null) {
                     single = new Singleton();// 2
+                }
             }
         }
         return single;
@@ -87,13 +86,12 @@ public class Singleton {
      * 加上volatile关键字，因为volatile可以禁止指令重排序。
      *********************************************************************** */
 
-
-    public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(20);
-        for (int i = 0; i< 20; i++) {
-            threadPool.execute(
-                    () -> System.out.println(Thread.currentThread().getName()+"  :"+Singleton.getInstance线程安全调优双重检查()));
-        }
-        threadPool.shutdown();
-    }
+//    public static void main(String[] args) {
+//        ExecutorService threadPool = Executors.newFixedThreadPool(20);
+//        for (int i = 0; i< 20; i++) {
+//            threadPool.execute(
+//                    () -> System.out.println(Thread.currentThread().getName()+"  :"+Singleton.getInstance线程安全调优双重检查()));
+//        }
+//        threadPool.shutdown();
+//    }
 }
