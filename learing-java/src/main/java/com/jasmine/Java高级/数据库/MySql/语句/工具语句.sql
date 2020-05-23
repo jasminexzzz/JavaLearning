ba204dfbb794e9f3494502346b8b21d1033d4e0d
wangyf <491548320@qq.com>
2020-05-23 09:43:50 +0800

    数据库

--- learing-java/src/main/java/com/jasmine/Java高级/数据库/MySql/语句/工具语句.sql
+++ learing-java/src/main/java/com/jasmine/Java高级/数据库/MySql/语句/工具语句.sql
@@ -18,9 +18,28 @@ from information_schema.tables
 where table_schema='myjavastudy'
 order by data_length desc, index_length desc;
 
--- 行专列
+-- 行转列
 select a.user_id,group_concat(a.device_id)
 from t_project_device_user a
 where a.delete_flag = 1
   and a.user_type = 2
 group by a.user_id
+
+/*
+判断某个字段是否在某个字符串中
+    如 ids 字段保存了 id : '1,2,3,4'
+       判断id 3 是否包含在字符串中使用 find_in_set 来查询
+
+如下例子 : 相同租户中, [1] 用户所拥有的角色
+      t_user                     t_role
+|user_id|tenant_id|       |id|tenant_id|user_ids|
+|   1   |    1    |       | 1|    1    |  1,2   |
+|   2   |    1    |       | 2|    1    |    2   |
+|   3   |    2    |       | 3|    2    |    3   |
+|   4   |    2    |       | 4|    2    |  3,4   |
+ */
+   select u.user_id,r.id,r.user_ids
+     from t_user u
+left join t_role r on r.tenant_id = u.tenant_id
+    where find_in_set(1,r.user_ids)
+
--- learing-java/src/main/java/com/jasmine/Java高级/数据库/MySql/语句/建表语句.sql
+++ learing-java/src/main/java/com/jasmine/Java高级/数据库/MySql/语句/建表语句.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS `base_menu`(
    `menu_name`  VARCHAR(100) NOT NULL COMMENT '名称',
    `level`      TINYINT      NOT NULL COMMENT '级别',
    `order`      TINYINT      NOT NULL COMMENT '排序',
-	 `states`     TINYINT      NOT NULL COMMENT '状态 0-关闭 1-启用',
+   `states`     TINYINT      NOT NULL COMMENT '状态 0-关闭 1-启用',
    PRIMARY KEY (`menu_id`)
 )
