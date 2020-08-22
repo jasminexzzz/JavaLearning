40ea9769b7d03d9ee918a49eebeb1bcebf5ffadd
wangyf <491548320@qq.com>
2020-08-22 14:45:12 +0800

    提交

---
+++ learning-kotlin/src/TestUser.java
@@ -0,0 +1,11 @@
+/**
+ * @author jasmineXz
+ */
+public class TestUser {
+
+    public static void main(String[] args) {
+        User u = new User();
+        u.setName("zxc");
+        System.out.println(u.getName());
+    }
+}
---
+++ learning-kotlin/src/User.kt
@@ -0,0 +1,5 @@
+open class User {
+
+    var name: String? = null
+    var id: Int? = null
+}
