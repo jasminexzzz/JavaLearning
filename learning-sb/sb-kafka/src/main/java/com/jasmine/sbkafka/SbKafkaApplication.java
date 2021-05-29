34b1c5097a83bc5e0d4e124571c54efe422f64b0
jasmineXz <491548320@qq.com>
2021-05-29 16:32:11 +0800

    结构变更

---
+++ learning-sb/sb-kafka/src/main/java/com/jasmine/sbkafka/SbKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.sbkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKafkaApplication.class, args);
+    }
+
+}
---
+++ learning-sb/sb-kafka/src/main/java/com/jasmine/sbkafka/kafka/DemoListener.java
@@ -0,0 +1,76 @@
+package com.jasmine.sbkafka.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author : jasmineXz
+ */
+@Component
+public class DemoListener implements Partitioner {
+    private static final Logger log= LoggerFactory.getLogger(DemoListener.class);
+
+    @KafkaListener(topics = "learningTopic1",groupId = "learning-consumer-group1")
+    public void topic1Listen1(
+            ConsumerRecord<?, ?> record,
+            Acknowledgment ack,
+            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            log.trace("topic1Listen1 - message : " + message);
+            ack.acknowledge();
+        }
+    }
+
+    @KafkaListener(topics = "learningTopic1",groupId = "learning-consumer-group2")
+    public void topic1Listen2(ConsumerRecord<?, ?> record,
+            Acknowledgment ack,
+          @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            log.trace("topic1Listen2 - message : " + message);
+            ack.acknowledge();
+        }
+    }
+
+    @Override
+    public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+        cluster.topics().forEach(item -> {
+            log.trace("topic个数" + item);
+        });
+        return 0;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+
+//    @KafkaListener(topics = "learningTopic2")
+//    public void listen2(ConsumerRecord<?, ?> record) {
+//        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+//        if (kafkaMessage.isPresent()) {
+//            Object message = kafkaMessage.get();
+//            System.out.println("learningTopic2 : message : " + message);
+//        }
+//    }
+
+}
