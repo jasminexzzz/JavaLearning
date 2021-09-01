b6cc197ceff7357ced767f04c6774a5f0f732f7b
jasmineXz <491548320@qq.com>
2021-09-01 20:15:37 +0800

    中间件学习模块

---
+++ learning-sb/sb-gateway/src/main/java/com/jasmine/gateway/SbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbGatewayApplication.class, args);
+    }
+
+}
---
+++ learning-sb/sb-skywalking/src/main/java/com/jasmine/sw/SbSkywalkingApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSkywalkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSkywalkingApplication.class, args);
+	}
+
+}
