ae0810e3d32917c13c126711423e19e8a55b85c2
jasmineXz <491548320@qq.com>
2021-11-29 20:36:40 +0800

    guava 学习

---
+++ learning-guava/guava-master/guava/src/com/google/zerolearning/SmoothWarmingUpTest.java
@@ -0,0 +1,12 @@
+package com.google.zerolearning;
+
+/**
+ * @author wangyf
+ * @since 2.0.0
+ */
+public class SmoothWarmingUpTest {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
