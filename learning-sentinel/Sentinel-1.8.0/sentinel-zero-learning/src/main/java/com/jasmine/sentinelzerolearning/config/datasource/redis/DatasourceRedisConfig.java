3bcd1d57efb91043e9ca67c8b534e0cc56181472
jasminexz <491548320@qq.com>
2021-12-30 00:10:01 +0800

    ✏ sentinel 学习

--- learning-sentinel/Sentinel-1.8.0/sentinel-zero-learning/src/main/java/com/jasmine/sentinelzerolearning/config/datasource/redis/DatasourceRedisConfig.java
+++ learning-sentinel/Sentinel-1.8.0/sentinel-zero-learning/src/main/java/com/jasmine/sentinelzerolearning/config/datasource/redis/DatasourceRedisConfig.java
@@ -1,29 +1,50 @@
 package com.jasmine.sentinelzerolearning.config.datasource.redis;
 
+import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
 import com.alibaba.csp.sentinel.datasource.redis.RedisDataSource;
 import com.alibaba.csp.sentinel.datasource.redis.config.RedisConnectionConfig;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
  * @author : jasmineXz
  */
+@Component
 public class DatasourceRedisConfig {
 
-    public static void init () {
+    private static final String FLOW_RULE_KEY = "sentinel:rule:flow";
+    private static final String FLOW_RULE_CHANNEL = "sentinel_channel_flow";
+
+    @PostConstruct
+    public void init() {
         RedisConnectionConfig redisConnectionConfig = buildRedisConnectionConfig();
-        ReadableDataSource<String, List<FlowRule>> redisDataSource = new RedisDataSource<List<FlowRule>>(redisConnectionConfig, ruleKey, channel, flowConfigParser);
+
+        ReadableDataSource<String, List<FlowRule>> redisDataSource = new RedisDataSource<List<FlowRule>>(
+                redisConnectionConfig,
+                FLOW_RULE_KEY,
+                FLOW_RULE_CHANNEL,
+                new ConverterTest());
+
         FlowRuleManager.register2Property(redisDataSource.getProperty());
     }
 
     private static RedisConnectionConfig buildRedisConnectionConfig () {
-        RedisConnectionConfig config = RedisConnectionConfig.builder()
+        return RedisConnectionConfig.builder()
             .withHost("localhost")
             .withPort(6379)
             .build();
-        return config;
+    }
+
+    public static class ConverterTest implements Converter {
+
+        @Override
+        public Object convert(Object source) {
+            return source;
+        }
     }
 }
