9e1def868c8bf210f1513589bd6d1e268c0df5fc
jasmineXz <491548320@qq.com>
2021-12-30 20:48:34 +0800

    ✏️ Gateway 学习

--- learning-sb/sb-gateway/src/main/java/com/jasmine/gateway/SbGatewayApplication.java
+++ learning-sb/sb-gateway/src/main/java/com/jasmine/gateway/SbGatewayApplication.java
@@ -1,13 +1,38 @@
 package com.jasmine.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SbGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SbGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(
+                "sentinel1",
+                r -> r.path("/sentinel/**")
+                    .and().weight("group1",1)
+                    .filters(f -> f.stripPrefix(1))
+                    .uri("http://localhost:12346")
+            )
+            .route(
+                "sentinel2",
+                r -> r.path("/sentinel/**")
+                    .and().weight("group1",1)
+                    .filters(f -> f.stripPrefix(1))
+                    .uri("http://localhost:12347")
+            )
+            .build();
     }
 
 }
