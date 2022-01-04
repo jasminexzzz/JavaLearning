efeb0ea35d1bc8db9e396094bf2b5384ed41f00b
jasminexz <491548320@qq.com>
2022-01-04 23:05:05 +0800

    ✏ sentinel 学习

--- learning-sentinel/Sentinel-1.8.0/sentinel-zero-learning/src/main/java/com/jasmine/sentinelzerolearning/controller/EntryController.java
+++ learning-sentinel/Sentinel-1.8.0/sentinel-zero-learning/src/main/java/com/jasmine/sentinelzerolearning/controller/EntryController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.context.ContextUtil;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.jasmine.sentinelzerolearning.util.MemoryAddrUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class EntryController {
         } finally {
             if (e1 != null) {
                 e1.exit();
-                printAddresses(ContextUtil.getContext());
+                MemoryAddrUtil.printAddresses(ContextUtil.getContext());
                 System.out.println(ContextUtil.getContext().getName());
             }
             ContextUtil.exit();
@@ -58,44 +59,5 @@ public class EntryController {
         }
     }
 
-
-    static final Unsafe unsafe = getUnsafe();
-    static final boolean is64bit = true;
-
-    public static void printAddresses(Object... objects) {
-        System.out.print("0x");
-        long last = 0;
-        int offset = unsafe.arrayBaseOffset(objects.getClass());
-        int scale = unsafe.arrayIndexScale(objects.getClass());
-        switch (scale) {
-            case 4:
-                long factor = is64bit ? 8 : 1;
-                final long i1 = (unsafe.getInt(objects, offset) & 0xFFFFFFFFL) * factor;
-                System.out.print(Long.toHexString(i1));
-                last = i1;
-                for (int i = 1; i < objects.length; i++) {
-                    final long i2 = (unsafe.getInt(objects, offset + i * 4) & 0xFFFFFFFFL) * factor;
-                    if (i2 > last)
-                        System.out.print(", +" + Long.toHexString(i2 - last));
-                    else
-                        System.out.print(", -" + Long.toHexString( last - i2));
-                    last = i2;
-                }
-                break;
-            case 8:
-                throw new AssertionError("Not supported");
-        }
-        System.out.println();
-    }
-
-    private static Unsafe getUnsafe() {
-        try {
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            return (Unsafe) theUnsafe.get(null);
-        } catch (Exception e) {
-            throw new AssertionError(e);
-        }
-    }
 
 }
---
+++ learning-sentinel/Sentinel-1.8.0/sentinel-zero-learning/src/main/java/com/jasmine/sentinelzerolearning/util/MemoryAddrUtil.java
@@ -0,0 +1,51 @@
+package com.jasmine.sentinelzerolearning.util;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author : jasmineXz
+ */
+public class MemoryAddrUtil {
+
+    private static final Unsafe unsafe = getUnsafe();
+    private static final boolean is64bit = true;
+
+    public static void printAddresses(Object... objects) {
+        System.out.print("0x");
+        long last = 0;
+        int offset = unsafe.arrayBaseOffset(objects.getClass());
+        int scale = unsafe.arrayIndexScale(objects.getClass());
+        switch (scale) {
+            case 4:
+                long factor = is64bit ? 8 : 1;
+                final long i1 = (unsafe.getInt(objects, offset) & 0xFFFFFFFFL) * factor;
+                System.out.print(Long.toHexString(i1));
+                last = i1;
+                for (int i = 1; i < objects.length; i++) {
+                    final long i2 = (unsafe.getInt(objects, offset + i * 4) & 0xFFFFFFFFL) * factor;
+                    if (i2 > last)
+                        System.out.print(", +" + Long.toHexString(i2 - last));
+                    else
+                        System.out.print(", -" + Long.toHexString( last - i2));
+                    last = i2;
+                }
+                break;
+            case 8:
+                throw new AssertionError("Not supported");
+        }
+        System.out.println();
+    }
+
+    private static Unsafe getUnsafe() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            return (Unsafe) theUnsafe.get(null);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+}
