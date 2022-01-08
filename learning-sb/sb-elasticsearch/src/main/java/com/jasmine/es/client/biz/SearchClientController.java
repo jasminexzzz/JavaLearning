5e34d645daa69d14fffa0b04e010262a2019a52e
jasmineXz <491548320@qq.com>
2022-01-08 18:14:19 +0800

    ✏️ es 学习

--- learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/client/biz/SearchClientController.java
+++ learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/client/biz/SearchClientController.java
@@ -10,6 +10,11 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
+import org.elasticsearch.search.aggregations.bucket.histogram.ParsedDateHistogram;
+import org.elasticsearch.search.aggregations.bucket.histogram.ParsedHistogram;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -57,6 +62,44 @@ public class SearchClientController {
         }
         return result;
     }
+
+    @GetMapping("/aggs")
+    public void aggs () {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(0);
+        final String field = "retailPrice";
+        AggregationBuilder aggs = AggregationBuilders
+            .histogram(field)
+            .field(field)
+            .interval(100000)
+            .missing(-1)
+            .minDocCount(0)
+            .extendedBounds(-1, 3000000)
+        ;
+        searchSourceBuilder.aggregation(aggs).explain(true);
+        // 查询数据
+        SearchResponse searchResponse = manager.originalSearch("index_item", searchSourceBuilder);
+        // 打印聚合类型
+        System.out.println(searchResponse.getAggregations().get(field).getClass().getName());
+        // 用来解析 Terms 聚合
+        ParsedHistogram histogram = searchResponse.getAggregations().get(field);
+        System.out.println("==========================================================================================================");
+        System.out.println("《Histogram 聚合的响应参数说明》\n");
+        System.out.println(String.format("\t聚合的类型: " +
+                        "\n\t\t1: histogram      (普通直方图聚合) : %s" +
+                        "\n\t\t2: date_histogram (时间直方图聚合) : %s" +
+                        "\n\t\t>  本次响应: %s",
+                ParsedHistogram.class.getName(),
+                ParsedDateHistogram.class.getName(),
+                histogram.getType()));
+        System.out.println("==========================================================================================================");
+
+        List<? extends Histogram.Bucket> buckets = histogram.getBuckets();
+        for (Histogram.Bucket bucket : buckets) {
+            System.out.println(String.format("count: % 5d, key: %s", bucket.getDocCount(), bucket.getKeyAsString()));
+        }
+    }
+
 
     @PostMapping
     public EsSearchDTO<ItemDTO> search (@RequestBody EsSearchDTO<ItemDTO> search) {
