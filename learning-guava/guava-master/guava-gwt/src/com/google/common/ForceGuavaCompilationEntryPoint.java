0cdc2ac1c1311e7b3aed72575cc13c1559ae7305
jasminexz <491548320@qq.com>
2022-01-19 21:44:42 +0800

    ✏ 注释掉GUAVA报错的类

--- learning-guava/guava-master/guava-gwt/src/com/google/common/ForceGuavaCompilationEntryPoint.java
+++ learning-guava/guava-master/guava-gwt/src/com/google/common/ForceGuavaCompilationEntryPoint.java
@@ -16,14 +16,14 @@
 
 package com.google.common;
 
-import com.google.gwt.core.client.EntryPoint;
+//import com.google.gwt.core.client.EntryPoint;
 
 /**
  * A dummy entry point to convince Maven to compile our classes.
  *
  * @author Chris Povirk
  */
-public class ForceGuavaCompilationEntryPoint implements EntryPoint {
-  @Override public void onModuleLoad() {
-  }
-}
+//public class ForceGuavaCompilationEntryPoint implements EntryPoint {
+//  @Override public void onModuleLoad() {
+//  }
+//}
