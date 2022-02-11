69d4c57ed7415ce893b6e32a7ce861988651cec1
jasmineXz <491548320@qq.com>
2022-02-11 11:44:07 +0800

    ✏️ es 学习

--- learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/repository/service/UserMapper.java
+++ learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/repository/service/UserMapper.java
@@ -1,13 +1,13 @@
-package com.jasmine.es.repository.service;
-
-import com.jasmine.es.repository.dto.UserDoc;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Component;
-
-/**
- * @author wangyf
- * @since 0.0.1
- */
-@Component
-public interface UserMapper extends ElasticsearchRepository<UserDoc,String> {
-}
+//package com.jasmine.es.repository.service;
+//
+//import com.jasmine.es.repository.dto.UserDoc;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author wangyf
+// * @since 0.0.1
+// */
+//@Component
+//public interface UserMapper extends ElasticsearchRepository<UserDoc,String> {
+//}
--- learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/template/GetController.java
+++ learning-sb/sb-elasticsearch/src/main/java/com/jasmine/es/template/GetController.java
@@ -1,31 +1,31 @@
-package com.jasmine.es.template;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-
-/**
- * @author wangyf
- * @since 0.0.1
- */
-@Slf4j
-@RestController
-@RequestMapping("/es")
-public class GetController {
-
-    @Autowired
-    private ElasticsearchRestTemplate es;
-
-    @RequestMapping("/get")
-    public Map<String, Object> get () {
-        IndexCoordinates indexCoordinates = IndexCoordinates.of("test_index");
-        return es.indexOps(indexCoordinates).getMapping();
-    }
-
-}
+//package com.jasmine.es.template;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+//import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Map;
+//
+//
+///**
+// * @author wangyf
+// * @since 0.0.1
+// */
+//@Slf4j
+//@RestController
+//@RequestMapping("/es")
+//public class GetController {
+//
+//    @Autowired
+//    private ElasticsearchRestTemplate es;
+//
+//    @RequestMapping("/get")
+//    public Map<String, Object> get () {
+//        IndexCoordinates indexCoordinates = IndexCoordinates.of("test_index");
+//        return es.indexOps(indexCoordinates).getMapping();
+//    }
+//
+//}
