7c0a7235d5287589c23a7d2ab225984a7321e16e
jasmineXz <491548320@qq.com>
2022-04-24 18:40:56 +0800

    ✏️ Spring 学习

--- learning-sb/sb-springboot/src/main/java/com/jasmine/sbspringboot/bean/beandefinition/BeanDefinitionGenericTest.java
+++ learning-sb/sb-springboot/src/main/java/com/jasmine/sbspringboot/bean/beandefinition/BeanDefinitionGenericTest.java
@@ -1,11 +1,17 @@
 package com.jasmine.sbspringboot.bean.beandefinition;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,7 +24,14 @@ import org.springframework.stereotype.Component;
     classes = {BeanDefinitionGenericTest.class},
     webEnvironment = SpringBootTest.WebEnvironment.NONE // 不启动WEB容器
 )
-public class BeanDefinitionGenericTest {
+public class BeanDefinitionGenericTest implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 
     /**
      * 第一种创建 BeanDefinition 的方式
@@ -26,14 +39,13 @@ public class BeanDefinitionGenericTest {
     @Test
     public void generic1() {
         // 1.通过 BeanDefinitionBuilder 构建
-        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
-                .genericBeanDefinition(BeanDefinitionTarget.class);
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(BeanDefinitionTarget.class);
 
         // 可以设置属性
-        beanDefinitionBuilder.addPropertyValue("name", "第一种创建方式");
+        builder.addPropertyValue("name", "第一种创建方式");
 
         // 获取 BeanDefinition 实例,并非 Bean 终态，可以自定义修改
-        BeanDefinition target = beanDefinitionBuilder.getBeanDefinition();
+        BeanDefinition target = builder.getBeanDefinition();
 
         // 打印
         System.out.println(target.getBeanClassName());
@@ -62,5 +74,31 @@ public class BeanDefinitionGenericTest {
 
         // 打印
         System.out.println(target.getBeanClassName());
+    }
+
+
+    /**
+     * 在创建时指定构造的执行方法
+     */
+    @Test
+    public void generic3() {
+
+        // 在创建 builder 时指定方法, 该放回会在真正创建对象时执行, 返回的对象即为在 IOC 中的 Bean 的实例
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(BeanDefinitionTarget.class, () -> {
+            BeanDefinitionTarget beanTarget = new BeanDefinitionTarget();
+            beanTarget.setName("第三种创建方式");
+            return beanTarget;
+        });
+
+
+        BeanDefinition target = builder.getBeanDefinition();
+
+        BeanDefinitionHolder holder = new BeanDefinitionHolder(target, BeanDefinitionTarget.class.getSimpleName());
+        BeanDefinitionReaderUtils.registerBeanDefinition(holder, (AnnotationConfigApplicationContext)applicationContext);
+
+        // 将 BeanDefinition 注册为 Bean 后, 可以查看 name 的变化
+        BeanDefinitionTarget bean = (BeanDefinitionTarget) applicationContext.getBean("BeanDefinitionTarget");
+        System.out.println(String.format("第二种方式注册 BeanDefinition, 并获取Bean: %s", bean.getName()));
+
     }
 }
